75d3830597c64ef701b251577c4e0e7abcad526b
sdzyryan <sdzyryan@gmail.com>
2023-04-12 21:30:31 +0500

    3-4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
 
     public static void task1 () {
@@ -52,4 +54,51 @@ public class Main {
             }
         }
         }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+        int[] massivOne = new int[3];
+        massivOne[0] = 1;
+        massivOne[1] = 2;
+        massivOne[2] = 3;
+        for (int i = massivOne.length - 1; i >= 0; i-- ) {
+            if (i > 0) {
+                System.out.print(massivOne[i] + ", ");
+            }
+            if (i == 0){
+                System.out.println(massivOne[i]);
+            }
+        }
+
+        double [] massivTwo = {1.57, 7.654, 9.986};
+        for (int i = massivTwo.length - 1; i >= 0; i-- ) {
+            if (i > 0) {
+                System.out.print(massivTwo[i] + ", ");
+            }
+            if (i == 0){
+                System.out.println(massivTwo[i]);
+            }
+        }
+
+        long [] massivThree = {7, 14, 21, 28};
+        for (int i = massivThree.length - 1; i >=0; i--) {
+            if (i > 0) {
+                System.out.print(massivThree[i] + ", ");
+            }
+            if (i == 0) {
+                System.out.println(massivThree[i]);
+            }
+        }
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 3");
+
+        int[] array = new int[3];
+        array[0] = 2;
+        array[1] = 4;
+        array[2] = 6;
+
+    }
 }
